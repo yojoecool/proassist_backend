aadfceefb5f3ace26aa72cdd10f95dbde78851b0
adamlui <wingfului@gmail.com>
2019-07-02 18:29:17 -0400

    added edit route and getjob route

--- routes/companies.js
+++ routes/companies.js
@@ -72,6 +72,51 @@ router.post('/addJob', verifyUser, async (req, res) => {
   }
 });
 
+router.get('/getJob', verifyUser, async (req, res) => {
+  if (req.locals.userId !== req.query.userId) {
+    res.status(403);
+    res.json({ success: false });
+    return;
+  }
+  console.log(req.query)
+
+  try {
+    const job = await companyFuncs.getJob(req.query.userId, req.query.jobId)
+    res.json({ success: true, job });
+  } catch (err) {   
+    if (err.message === 'Job does not exist'){
+      res.status(404);
+      res.json({ success: false });
+      return;
+    }
+    res.status(500);
+    res.json({ success: false });
+  }
+});
+
+router.put('/editJob', verifyUser, async (req, res) => {
+  if (req.locals.userId !== req.query.userId) {
+    res.status(403);
+    res.json({ success: false });
+    return;
+  }
+
+  try {
+
+    await companyFuncs.editJob(req.query.userId, req.query.jobId, req.body)
+    res.json({ success: true });
+
+  } catch (err) {
+    console.log(err);
+    if (err.message === 'Company is not Active') {
+      res.status(403);
+    } else {
+      res.status(500);
+    }
+    res.json({ success: false });
+  }
+});
+
 router.get('/getJobs', verifyUser, async (req, res) => {
     if (req.locals.userId !== req.query.userId) {
       res.status(403);
